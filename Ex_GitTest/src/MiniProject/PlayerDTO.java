e185fe9f93ff8124fa6834186c506eb0bd6e8957
smhrd <smhrd@DESKTOP-KF60LL1>
2023-05-10 15:32:53 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-SW-BigData-1/i-am-groot.git

--- Ex_GitTest/src/MiniProject/PlayerDTO.java
+++ Ex_GitTest/src/MiniProject/PlayerDTO.java
@@ -2,4 +2,47 @@ package MiniProject;
 
 public class PlayerDTO {
 
+	private String ID;
+	private String PW;
+	private String NICK;
+	
+	
+	
+	public PlayerDTO(String iD, String pW, String nICK) {
+		super();
+		ID = iD;
+		PW = pW;
+		NICK = nICK;
+	}
+	
+	
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getPW() {
+		return PW;
+	}
+
+	public void setPW(String pW) {
+		PW = pW;
+	}
+
+	public String getNICK() {
+		return NICK;
+	}
+
+	public void setNICK(String nICK) {
+		NICK = nICK;
+	}
+
+	
+	
+	
+	
+	
 }
