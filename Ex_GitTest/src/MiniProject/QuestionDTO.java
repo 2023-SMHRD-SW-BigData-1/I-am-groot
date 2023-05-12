12ea4e972c05c0c6a0c94a49f57e0874c8e95c83
smhrd <smhrd@DESKTOP-KF60LL1>
2023-05-12 17:32:51 +0900

    희주 - 퀘스천디티오 햇스여 확인하고 이상있으면 고지 후 변경 바람다

--- Ex_GitTest/src/MiniProject/QuestionDTO.java
+++ Ex_GitTest/src/MiniProject/QuestionDTO.java
@@ -1,5 +1,35 @@
 package MiniProject;
 
+import java.util.Objects;
+
 public class QuestionDTO {
 
+	private String QS ; 
+	private String ANSWER;
+	
+	
+
+	public QuestionDTO(String qS, String aNSWER) {
+		super();
+		QS = qS;
+		ANSWER = aNSWER;
+	}
+
+	public String getQS() {
+		return QS;
+	}
+
+	public void setQS(String qS) {
+		QS = qS;
+	}
+
+	public String getANSWER() {
+		return ANSWER;
+	}
+
+	public void setANSWER(String aNSWER) {
+		ANSWER = aNSWER;
+	} 
+	
+	
 }
